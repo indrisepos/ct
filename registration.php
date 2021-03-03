ed7d4869ec594a8e4b7add54359e176a5fa17abc
Sipos András <andras.sipos@mzperx.net>
2021-03-03 18:15:06 +0100

    first release

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © MZperX Kft All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'MZperX_CT', __DIR__);
+
